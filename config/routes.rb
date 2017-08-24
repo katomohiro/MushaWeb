63af2ae3d1be98232c65f8998ee66928ace448e3
Tomohiro Kato <k.t.football21@gmail.com>
2017-08-24 09:51:11 +0000

    Personでdevise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :people, controllers: {
+  sessions:      'people/sessions',
+  passwords:     'people/passwords',
+  registrations: 'people/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
   root 'static_pages#home'
 
   # Example of regular route:
