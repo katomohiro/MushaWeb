96bea5c65d3eec58bbbeddabad765a71e924e735
Tomohiro Kato <k.t.football21@gmail.com>
2017-10-02 07:24:45 +0000

    内定者入力欄を作成

--- app/controllers/people/registrations_controller.rb
+++ app/controllers/people/registrations_controller.rb
@@ -44,7 +44,7 @@ class People::RegistrationsController < Devise::RegistrationsController
   end
 
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :area_id, :other_naitei])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :area_id, :other_naitei, :is_student])
   end
 
   # The path used after sign up.
