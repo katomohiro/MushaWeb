6a0a57c646d25f3681bff4eaaa6ba2e4b6c64999
Tomohiro Kato <k.t.football21@gmail.com>
2017-10-03 07:30:02 +0000

    schema.rbが壊れてたので修正

--- db/schema.rb
+++ db/schema.rb
@@ -13,57 +13,57 @@
 ActiveRecord::Schema.define(version: 20171002083924) do
 
   create_table "areas", force: :cascade do |t|
-    t.string   "name"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "region"
+    t.string "region"
   end
 
   create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.text     "content"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "title"
+    t.text "description"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "articles_industries", id: false, force: :cascade do |t|
-    t.integer "article_id",  null: false
+    t.integer "article_id", null: false
     t.integer "industry_id", null: false
     t.index ["article_id"], name: "index_articles_industries_on_article_id"
     t.index ["industry_id"], name: "index_articles_industries_on_industry_id"
   end
 
   create_table "articles_occupations", id: false, force: :cascade do |t|
-    t.integer "article_id",    null: false
+    t.integer "article_id", null: false
     t.integer "occupation_id", null: false
     t.index ["article_id"], name: "index_articles_occupations_on_article_id"
     t.index ["occupation_id"], name: "index_articles_occupations_on_occupation_id"
   end
 
   create_table "career_industries", force: :cascade do |t|
-    t.integer  "industry_id"
-    t.integer  "career_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer "industry_id"
+    t.integer "career_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["career_id"], name: "index_career_industries_on_career_id"
     t.index ["industry_id"], name: "index_career_industries_on_industry_id"
   end
 
   create_table "career_occupations", force: :cascade do |t|
-    t.integer  "occupation_id"
-    t.integer  "career_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.integer "occupation_id"
+    t.integer "career_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["career_id"], name: "index_career_occupations_on_career_id"
     t.index ["occupation_id"], name: "index_career_occupations_on_occupation_id"
   end
 
   create_table "careers", force: :cascade do |t|
-    t.integer  "person_id"
-    t.string   "company_name"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.integer "person_id"
+    t.string "company_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "careers_industries", force: :cascade do |t|
@@ -81,89 +81,89 @@ ActiveRecord::Schema.define(version: 20171002083924) do
   end
 
   create_table "contacts", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "person_id"
-    t.text     "message"
+    t.integer "user_id"
+    t.integer "person_id"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "industries", force: :cascade do |t|
-    t.string   "name",       null: false
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "industries_users", id: false, force: :cascade do |t|
     t.integer "industry_id", null: false
-    t.integer "user_id",     null: false
+    t.integer "user_id", null: false
     t.index ["industry_id"], name: "index_industries_users_on_industry_id"
     t.index ["user_id"], name: "index_industries_users_on_user_id"
   end
 
   create_table "industry_users", force: :cascade do |t|
-    t.integer  "industry_id"
-    t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer "industry_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["industry_id"], name: "index_industry_users_on_industry_id"
     t.index ["user_id"], name: "index_industry_users_on_user_id"
   end
 
   create_table "occupations", force: :cascade do |t|
-    t.string   "name",       null: false
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "occupations_users", id: false, force: :cascade do |t|
     t.integer "occupation_id", null: false
-    t.integer "user_id",       null: false
+    t.integer "user_id", null: false
     t.index ["occupation_id"], name: "index_occupations_users_on_occupation_id"
     t.index ["user_id"], name: "index_occupations_users_on_user_id"
   end
 
   create_table "people", force: :cascade do |t|
-    t.string   "name",                   default: "", null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.integer  "area_id"
-    t.text     "other_naitei"
-    t.boolean  "is_student"
-    t.string   "musha_term"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "area_id"
+    t.text "other_naitei"
+    t.boolean "is_student"
+    t.string "musha_term"
     t.index ["email"], name: "index_people_on_email", unique: true
     t.index ["reset_password_token"], name: "index_people_on_reset_password_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "name"
-    t.integer  "admin"
-    t.integer  "area_id"
-    t.string   "musha_term"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.integer "admin"
+    t.integer "area_id"
+    t.string "musha_term"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
